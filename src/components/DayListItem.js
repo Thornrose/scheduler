06f38799d027e57acadd379e80c1f54a238b3daf
Cheever Esler <ames.esler@gmail.com>
2022-11-30 18:23:52 -0400

    add DayList and DayListItem components with stories and tests

---
+++ src/components/DayListItem.js
@@ -0,0 +1,40 @@
+import React from "react";
+import classNames from "classnames";
+
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  const { name, spots, selected, setDay} = { ...props };
+
+  const itemClass = classNames("day-list__item", {
+    "day-list__item--selected": selected,
+    "day-list__item--full": (spots === 0)
+  });
+
+  const clickHandler = () => (setDay(name));
+
+  const formatSpots = function(spots) {
+    let spotString = "";
+    if (spots === 0) {
+      spotString += "no spots remaining";
+    } else if (spots === 1) {
+      spotString += "1 spot remaining";
+    } else {
+      spotString += `${spots} spots remaining`;
+    }
+    return spotString;
+  }
+
+  const formattedSpots = formatSpots(spots);  
+
+  return (
+    <li 
+    className={itemClass}
+      selected={selected} 
+      onClick={clickHandler}
+      >
+      <h2 className="text--regular">{name}</h2>
+      <h3 className="text-light">{formattedSpots}</h3>
+    </li>
+  );
+}
