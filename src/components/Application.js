d9aeb02c3576d6899edb1e4e50f5cfad89655c43
Cheever Esler <ames.esler@gmail.com>
2022-12-02 17:21:16 -0400

    add Appointment to Application

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import DayList from "./DayList";
+import Appointment from "./Appointment";
 
 import "components/Application.scss";
 
-// Mock data
+// Mock days data
 const days = [
   {
     id: 1,
@@ -22,10 +23,60 @@ const days = [
   },
 ];
 
+// Mock appointments data
+
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
+
 
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+
+  const mockAppointments = Object.values(appointments).map((appointment) => {
+    return (
+      <Appointment
+        key={appointment.id}
+        {...appointment}
+      />
+    )
+  })
   
   return (
     <main className="layout">
@@ -50,7 +101,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements during the "The Scheduler" activity. */}
+        {mockAppointments}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
